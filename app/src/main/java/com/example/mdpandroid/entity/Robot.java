6e1e8daa1430a81864eee8ba72a680219dd497b5
Kenneth Soh <sohy0037@e.ntu.edu.sg>
2020-03-02 11:46:14 +0800

    Lets go kotlin HAHAHHA

--- app/src/main/java/com/example/mdpandroid/entity/Robot.java
+++
@@ -1,7 +0,0 @@
-package com.example.mdpandroid.entity;
-
-public class Robot {
-    public static final int START_POS_X = 1;
-    public static final int START_POS_Y = 18;
-    public static final String START_DIRECTION = "Right";
-}
